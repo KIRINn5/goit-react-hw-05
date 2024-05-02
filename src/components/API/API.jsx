02abb868668cca819040b1b06365008bc8d63e1c
KIRINn5 <141363578+KIRINn5@users.noreply.github.com>
2024-05-02 12:37:09 +0300

    update

--- src/components/API/API.jsx
+++ src/components/API/API.jsx
@@ -1,9 +1,9 @@
+import { API_READ_ACCESS_TOKEN } from "шлях_до_файлу_де_експортується_API_READ_ACCESS_TOKEN";
 import axios from "axios";
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
-    // eslint-disable-next-line no-undef
     Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`,
   },
 });
