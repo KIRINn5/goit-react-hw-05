4919f959f65ef099d8832ef98b52484186e38353
KIRINn5 <141363578+KIRINn5@users.noreply.github.com>
2024-05-02 14:14:43 +0300

    update

--- src/components/API/API.jsx
+++ src/components/API/API.jsx
@@ -1,5 +1,5 @@
-import { API_READ_ACCESS_TOKEN } from "./API";
 import axios from "axios";
+import { API_READ_ACCESS_TOKEN } from "./API";
 
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
@@ -8,7 +8,52 @@ const instance = axios.create({
   },
 });
 
-instance
-  .get("/trending/movie/day?language=en-US")
-  .then((response) => {})
-  .catch((error) => {});
+export const fetchHomePage = async () => {
+  try {
+    const response = await instance.get("/trending/movie/day?language=en-US");
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching homepage:", error);
+    throw error;
+  }
+};
+
+export const getMoviesPage = async (query) => {
+  try {
+    const response = await instance.get(`/search/movie?query=${query}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching movies page:", error);
+    throw error;
+  }
+};
+
+export const getMovieDetails = async (id) => {
+  try {
+    const response = await instance.get(`/movie/${id}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching movie details:", error);
+    throw error;
+  }
+};
+
+export const getMovieReviews = async (id) => {
+  try {
+    const response = await instance.get(`/movie/${id}/reviews`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching movie reviews:", error);
+    throw error;
+  }
+};
+
+export const getMovieCast = async (id) => {
+  try {
+    const response = await instance.get(`/movie/${id}/credits`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching movie cast:", error);
+    throw error;
+  }
+};
