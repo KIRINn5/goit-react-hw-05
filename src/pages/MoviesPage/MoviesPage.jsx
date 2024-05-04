d5a07fc3c55ff3981f4d390738747e52923774b6
KIRINn5 <141363578+KIRINn5@users.noreply.github.com>
2024-05-04 14:29:14 +0300

    update

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,91 +1,50 @@
+/* eslint-disable no-undef */
 import { useState, useEffect } from "react";
-import axios from "axios";
-import MovieList from "../../components/MovieList/MovieList";
-import { Formik, Field, Form } from "formik";
+import { useSearchParams } from "react-router-dom";
+import { getMoviesPage } from "../../components/API/API";
+import ErrorMessage from "../components/ErrorMessage/ErrorMessage";
+import Loader from "../components/Loader/Loader";
+import MovieList from "../components/MovieList/MovieList";
 import toast, { Toaster } from "react-hot-toast";
+import { Formik, Field, Form } from "formik";
 
 const MoviesPage = () => {
   const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get("query");
 
   useEffect(() => {
-    const fetchMovies = async () => {
+    if (!query) return;
+    async function fetchSearchMovie() {
       try {
-        setLoading(true);
-        const url = `https://api.themoviedb.org/3/discover/movie`;
-        const params = {
-          api_key:
-            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDc0OGRiNTVhMThjN2QxNzg1ODkzOTE0YjY1OWViNCIsInN1YiI6IjY2MGVlN2I4NWFhZGM0MDE0OTYzNTI1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EwV41cGxMc2HGH9eWewv2gmR2sjoGuKP7LPcQkG3QtM",
-          language: "en-US",
-          sort_by: "popularity.desc",
-          include_adult: false,
-          page: 1,
-        };
-
-        const response = await axios.get(url, { params });
-        setMovies(response.data.results);
-        setLoading(false);
-        setError(null);
+        setIsError(false);
+        setIsLoading(true);
+        const response = await getMoviesPage(query);
+        setMovies(response.results);
+        if (response.total_results === 0) {
+          message();
+        }
       } catch (error) {
-        console.error("Error fetching movies:", error);
-        setError("Error fetching movies");
-        setLoading(false);
+        console.log("error: ", error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
       }
-    };
-    fetchMovies();
-  }, []);
+    }
+    fetchSearchMovie();
+  }, [query]);
 
-  const handleSubmit = async (values) => {
-    const { query } = values;
-    if (!query.trim()) {
-      toast.error("This field cannot be empty. Please enter a search query", {
-        duration: 4000,
-        position: "top-left",
-        style: {
-          borderRadius: "10px",
-          background: "#333",
-          color: "#fff",
-        },
-      });
-      return;
-    }
-    try {
-      setLoading(true);
-      const url = `https://api.themoviedb.org/3/search/movie`;
-      const params = {
-        api_key:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDc0OGRiNTVhMThjN2QxNzg1ODkzOTE0YjY1OWViNCIsInN1YiI6IjY2MGVlN2I4NWFhZGM0MDE0OTYzNTI1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EwV41cGxMc2HGH9eWewv2gmR2sjoGuKP7LPcQkG3QtM",
-        include_adult: false,
-        language: "en-US",
-        page: 1,
-        query: query,
-      };
-      const response = await axios.get(url, { params });
-      setMovies(response.data.results);
-      if (response.data.results.length === 0) {
-        toast.error("There are no movies. Please enter another request", {
-          duration: 4000,
-          position: "top-left",
-          style: {
-            borderRadius: "10px",
-            background: "#387ce1",
-            color: "#fff",
-          },
-        });
-      }
-      setLoading(false);
-      setError(null);
-    } catch (error) {
-      console.error("Error fetching movies:", error);
-      setError("Error fetching movies");
-      setLoading(false);
-    }
+  const handleSubmit = (value) => {
+    if (!value.query.trim()) return notify();
+    if (query === value.query) return;
+    setSearchParams({ query: value.query });
   };
 
   return (
-    <div>
-      <h1>Movies</h1>
+    <>
       <Formik initialValues={{ query: "" }} onSubmit={handleSubmit}>
         <Form>
           <label>
@@ -101,9 +60,10 @@ const MoviesPage = () => {
           <Toaster />
         </Form>
       </Formik>
-      {loading ? <div>Loading...</div> : <MovieList movies={movies} />}
-      {error && <div>Error: {error}</div>}
-    </div>
+      {movies && <MovieList movies={movies} />}
+      {isError && <ErrorMessage />}
+      {isLoading && <Loader />}
+    </>
   );
 };
 
