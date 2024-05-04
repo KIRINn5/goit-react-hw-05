195fe9ba717960708f907aa68b460e12f3df993f
KIRINn5 <141363578+KIRINn5@users.noreply.github.com>
2024-05-04 15:40:03 +0300

    update

--- src/components/API/API.jsx
+++ src/components/API/API.jsx
@@ -6,7 +6,7 @@ const API_READ_ACCESS_TOKEN =
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   headers: {
-    Authorization: `${API_READ_ACCESS_TOKEN}`,
+    Authorization: `Bearer ${API_READ_ACCESS_TOKEN}`,
   },
 });
 
