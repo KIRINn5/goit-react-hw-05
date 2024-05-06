a9e5c72ac4d026a2af95a69f022f8c62be14040a
KIRINn5 <141363578+KIRINn5@users.noreply.github.com>
2024-05-06 03:16:12 +0300

    update

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -1,9 +1,14 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, Suspense, lazy } from "react";
 import { useParams, useLocation, Link, Routes, Route } from "react-router-dom";
 import axios from "axios";
 import ErrorMessage from "../../components/ErrorMessage/ErrorMessage";
 import Loader from "../../components/Loader/Loader";
 import css from "./MovieDetailsPage.module.css";
+
+const MovieCast = lazy(() => import("../../components/MovieCast/MovieCast"));
+const MovieReviews = lazy(() =>
+  import("../../components/MovieReviews/MovieReviews")
+);
 
 const MovieDetailsPage = () => {
   const [movieDetails, setMovieDetails] = useState(null);
@@ -15,59 +20,49 @@ const MovieDetailsPage = () => {
   const { movieId } = useParams();
   const location = useLocation();
   const backLink = useRef(location.state ?? "/");
+  const apiKey =
+    "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NDc0OGRiNTVhMThjN2QxNzg1ODkzOTE0YjY1OWViNCIsInN1YiI6IjY2MGVlN2I4NWFhZGM0MDE0OTYzNTI1NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EwV41cGxMc2HGH9eWewv2gmR2sjoGuKP7LPcQkG3QtM";
+
+  const fetchMovieData = async (url, setDataCallback) => {
+    try {
+      setIsLoading(true);
+      const response = await axios.get(`${url}?api_key=${apiKey}`);
+      setDataCallback(response.data);
+      setIsError(false);
+    } catch (error) {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchMovieDetails = async () => {
-      setIsLoading(true);
-      try {
-        const url = `https://api.themoviedb.org/3/movie/${movieId}`;
-        const response = await axios.get(url);
-        setMovieDetails(response.data);
-        setIsError(false);
-      } catch (error) {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    const fetchMovieReviews = async () => {
-      try {
-        const url = `https://api.themoviedb.org/3/movie/${movieId}/reviews`;
-        const response = await axios.get(url);
-        setReviews(response.data.results);
-      } catch (error) {
-        setIsError(true);
-      }
-    };
-
-    const fetchMovieCast = async () => {
-      try {
-        const url = `https://api.themoviedb.org/3/movie/${movieId}/credits`;
-        const response = await axios.get(url);
-        setCast(response.data.cast);
-      } catch (error) {
-        setIsError(true);
-      }
-    };
-
     prevLocation.current = location.state;
-    fetchMovieDetails();
-    fetchMovieReviews();
-    fetchMovieCast();
-  }, [movieId, location]);
+    fetchMovieData(
+      `https://api.themoviedb.org/3/movie/${movieId}`,
+      setMovieDetails
+    );
+    fetchMovieData(
+      `https://api.themoviedb.org/3/movie/${movieId}/reviews`,
+      setReviews
+    );
+    fetchMovieData(
+      `https://api.themoviedb.org/3/movie/${movieId}/credits`,
+      setCast
+    );
+  }, [movieId, location, apiKey]);
 
   return (
     <>
-      {isLoading && <Loader />}
-      {isError && <ErrorMessage />}
-      {!isLoading && !isError && (
-        <>
-          {movieDetails && (
-            <>
-              <Link to={backLink.current}>⬅ Go Back</Link>
-              <div>
-                <h1>{movieDetails.original_title} </h1>
+      <div>
+        {isLoading && <Loader />}
+        {isError && <ErrorMessage />}
+        {!isLoading && !isError && (
+          <>
+            {movieDetails && (
+              <>
+                <Link to={backLink.current}>⬅ Go Back</Link>
+                <h1>{movieDetails.original_title}</h1>
                 <img
                   src={`https://image.tmdb.org/t/p/w500${movieDetails.backdrop_path}`}
                   alt={movieDetails.title}
@@ -82,23 +77,28 @@ const MovieDetailsPage = () => {
                     ))}
                   </ul>
                 </div>
-              </div>
-              <div className={css.moviedetails}>
-                <h4>Additional Information</h4>
-                <Link to={`${location.pathname}/cast`}>Cast</Link>
-                <Link to={`${location.pathname}/reviews`}>Reviews</Link>
-                <Routes>
-                  <Route path="cast" element={<MovieCast cast={cast} />} />
-                  <Route
-                    path="reviews"
-                    element={<MovieReviews reviews={reviews} />}
-                  />
-                </Routes>
-              </div>
-            </>
-          )}
-        </>
-      )}
+              </>
+            )}
+          </>
+        )}
+      </div>
+      <div className={css.moviedetails}>
+        <h4>Additional Information</h4>
+        <Suspense fallback={<Loader />}>
+          <Routes>
+            <Route
+              path={`${location.pathname}/cast`}
+              element={<MovieCast cast={cast} />}
+            />
+            <Route
+              path={`${location.pathname}/reviews`}
+              element={<MovieReviews reviews={reviews} />}
+            />
+          </Routes>
+        </Suspense>
+        <Link to={`${location.pathname}/cast`}>Cast</Link>
+        <Link to={`${location.pathname}/reviews`}>Reviews</Link>
+      </div>
     </>
   );
 };
